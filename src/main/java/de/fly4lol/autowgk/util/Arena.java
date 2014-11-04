d56e49f4525dc84cb867780cee48d740937daeca
fly4lol <f.pohli.1@gmx.de>
2014-11-04 20:09:40 +0100

    We have now a Arena class. Now Realy!

--- src/main/java/de/fly4lol/autowgk/util/Arena.java
+++ src/main/java/de/fly4lol/autowgk/util/Arena.java
@@ -18,4 +18,44 @@ public class Arena {
 		
 	}
 
+	public Location getTeam1Loc() {
+		return team1Loc;
+	}
+
+	public void setTeam1Loc(Location team1Loc) {
+		this.team1Loc = team1Loc;
+	}
+
+	public Location getTeam2Loc() {
+		return team2Loc;
+	}
+
+	public void setTeam2Loc(Location team2Loc) {
+		this.team2Loc = team2Loc;
+	}
+
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	public String getTeam1Location() {
+		return team1Location;
+	}
+
+	public void setTeam1Location(String team1Location) {
+		this.team1Location = team1Location;
+	}
+
+	public String getTeam2Location() {
+		return team2Location;
+	}
+
+	public void setTeam2Location(String team2Location) {
+		this.team2Location = team2Location;
+	}
+	
 }
