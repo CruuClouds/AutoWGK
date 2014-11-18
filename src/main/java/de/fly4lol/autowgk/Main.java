ec1ce669a5e37dfb21b3ffd77671e06fa7ab7248
fly4lol <f.pohli.1@gmx.de>
2014-11-18 22:22:00 +0100

    bugfixes onCommand Method

--- src/main/java/de/fly4lol/autowgk/Main.java
+++ src/main/java/de/fly4lol/autowgk/Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -48,6 +50,10 @@ public class Main extends JavaPlugin{
 
 	public void MySQL(MySQLMethods sql) {
 		this.sql = sql;
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		return this.framework.handleCommand(sender, label, cmd, args);
 	}
 
 
