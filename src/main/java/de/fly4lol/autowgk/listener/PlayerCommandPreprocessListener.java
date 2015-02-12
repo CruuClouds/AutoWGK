39485c4d4934e446e9676dd1be93ef40af9710ec
Postremus <postremus1996@googlemail.com>
2015-02-12 19:58:15 +0100

    The Team knows in which team it is

--- src/main/java/de/fly4lol/autowgk/listener/PlayerCommandPreprocessListener.java
+++ src/main/java/de/fly4lol/autowgk/listener/PlayerCommandPreprocessListener.java
@@ -9,6 +9,7 @@ import de.fly4lol.autowgk.fightmanager.Team;
 import de.pro_crafting.wg.arena.Arena;
 import de.pro_crafting.wg.arena.State;
 import de.pro_crafting.wg.group.GroupSide;
+import de.pro_crafting.wg.group.PlayerRole;
 
 public class PlayerCommandPreprocessListener implements Listener{
 	private Main plugin;
@@ -22,20 +23,18 @@ public class PlayerCommandPreprocessListener implements Listener{
 		if(event.getMessage().equalsIgnoreCase("/wgk team leave")){
 			Team team = plugin.getUtil().getTeamByPlayer( event.getPlayer() );
 			Arena wgkArena = team.getAutoArena().getWgkArena();
-			if(team != null && team.getAutoArena().getWgkArena().getState() == State.Setup){
-				if(plugin.getUtil().getTeamByPlayer( event.getPlayer() ) != null ) {	
-					GroupSide side = GroupSide.Team2;
-					if(team.getAutoArena().getTeam1() == team){
-						side = GroupSide.Team1;
-						team.getAutoArena().setTeam1( null);
-						
-					} else {
-						team.getAutoArena().setTeam2( null );
-					}
-					team.getAutoArena().getWgkArena().getReseter().cleanSide( side );
-					this.plugin.wg.getScoreboard().removeTeamMember(wgkArena  , wgkArena.getGroupManager().getGroupMember( event.getPlayer()), wgkArena.getGroupManager().getRole( event.getPlayer()));
-					wgkArena.getGroupManager().getGroupOfPlayer( event.getPlayer() ).remove( event.getPlayer() );;
+			if(team != null && team.getAutoArena().getWgkArena().getState() == State.Setup && team != null ){
+				GroupSide side = GroupSide.Team2;
+				if(team.getRole() == PlayerRole.Team1){
+					side = GroupSide.Team1;
+					team.getAutoArena().setTeam1( null);
+					
+				} else {
+					team.getAutoArena().setTeam2( null );
 				}
+				team.getAutoArena().getWgkArena().getReseter().cleanSide( side );
+				this.plugin.wg.getScoreboard().removeTeamMember(wgkArena  , wgkArena.getGroupManager().getGroupMember( event.getPlayer()), wgkArena.getGroupManager().getRole( event.getPlayer()));
+				wgkArena.getGroupManager().getGroupOfPlayer( event.getPlayer() ).remove( event.getPlayer() );;
 			}
 		}
 	}
