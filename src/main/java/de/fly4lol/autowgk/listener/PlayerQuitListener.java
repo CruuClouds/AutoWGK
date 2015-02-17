0887234199418c1820a546e387bab52bfd1cf99d
Postremus <postremus1996@googlemail.com>
2015-02-17 17:33:23 +0100

    Teams will be replaced in the idle state

--- src/main/java/de/fly4lol/autowgk/listener/PlayerQuitListener.java
+++ src/main/java/de/fly4lol/autowgk/listener/PlayerQuitListener.java
@@ -21,44 +21,31 @@ public class PlayerQuitListener implements Listener{
 	@EventHandler
 	public void playerQuitHandler(PlayerQuitEvent event) {
 		Team team = plugin.getUtil().getTeamByPlayer( event.getPlayer() );
-		Arena wgkArena = null;
 		if (team == null) {
 			return;
 		}
+		Arena wgkArena = team.getAutoArena().getWgkArena();
 		
-		wgkArena = team.getAutoArena().getWgkArena();
-		if(team.getAutoArena().getTeam1() == team){
-			team.getAutoArena().setTeam1( null);
-			return;
-		}
-		
-		if(team.getAutoArena().getTeam2() == team){
-			team.getAutoArena().setTeam2( null);
-			return;
-		}
-		
-		if(team.getAutoArena().getWgkArena().getState() == State.Setup){
-			if(wgkArena.getState() == State.Idle){
-				if(team.getRole() == PlayerRole.Team1){
-					team.getAutoArena().setTeam1( team);
-				} else if(team.getRole() == PlayerRole.Team2){
-					team.getAutoArena().setTeam2( team );
-				}
-			} else if(wgkArena.getState() == State.Setup){
-				GroupSide side = GroupSide.Team2;
-				if(team.getAutoArena().getTeam1() == team){
-					side = GroupSide.Team1;
-					team.getAutoArena().setTeam1( null);
-					
-				} else {
-					team.getAutoArena().setTeam2( null );
-				}
-				if(wgkArena != null){
-					team.getAutoArena().getWgkArena().getReseter().cleanSide(side);
-					this.plugin.getRepo().getWarGear().getScoreboard().removeTeamMember(wgkArena, wgkArena.getGroupManager().getGroupMember(event.getPlayer()), team.getRole());
-					wgkArena.getGroupManager().getGroupOfPlayer(event.getPlayer()).remove(event.getPlayer());
-				}	
+		if(wgkArena.getState() == State.Idle){
+			if(team.getRole() == PlayerRole.Team1){
+				team.getAutoArena().setTeam1( team);
+			} else if(team.getRole() == PlayerRole.Team2){
+				team.getAutoArena().setTeam2( team );
 			}
+		} else if(wgkArena.getState() == State.Setup){
+			GroupSide side = GroupSide.Team2;
+			if(team.getAutoArena().getTeam1() == team){
+				side = GroupSide.Team1;
+				team.getAutoArena().setTeam1( null);
+				
+			} else {
+				team.getAutoArena().setTeam2( null );
+			}
+			
+			team.getAutoArena().getWgkArena().getReseter().cleanSide(side);
+			this.plugin.getRepo().getWarGear().getScoreboard().removeTeamMember(wgkArena, wgkArena.getGroupManager().getGroupMember(event.getPlayer()), team.getRole());
+			wgkArena.getGroupManager().getGroupOfPlayer(event.getPlayer()).remove(event.getPlayer());
+			
 		}
 	}
 }
