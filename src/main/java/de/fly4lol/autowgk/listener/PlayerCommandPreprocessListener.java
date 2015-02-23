4a9772b4cd62ccebf7b2bbd416608ef33c9c22dd
Postremus <postremus1996@googlemail.com>
2015-02-23 15:02:16 +0100

    fixed wgk team leave

--- src/main/java/de/fly4lol/autowgk/listener/PlayerCommandPreprocessListener.java
+++ src/main/java/de/fly4lol/autowgk/listener/PlayerCommandPreprocessListener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 import de.fly4lol.autowgk.Main;
 import de.fly4lol.autowgk.arena.Team;
+import de.pro_crafting.wg.arena.Arena;
 import de.pro_crafting.wg.arena.State;
 import de.pro_crafting.wg.group.GroupSide;
 import de.pro_crafting.wg.group.PlayerRole;
@@ -21,16 +22,25 @@ public class PlayerCommandPreprocessListener implements Listener{
 	public void playerCommandHandler(PlayerCommandPreprocessEvent event) {
 		if(event.getMessage().equalsIgnoreCase("/wgk team leave")){
 			Team team = plugin.getArenenManager().getTeamByPlayer(event.getPlayer());
-			if(team != null && team.getAutoArena().getWgkArena().getState() == State.Setup){
-				GroupSide side = GroupSide.Team2;
+			if (team == null) {
+				return;
+			}
+			
+			Arena wgkArena = team.getAutoArena().getWgkArena();
+			
+			if(wgkArena.getState() == State.Idle || wgkArena.getState() == State.Setup){
 				if(team.getRole() == PlayerRole.Team1){
-					side = GroupSide.Team1;
-					team.getAutoArena().setTeam1( null);
-					
-				} else {
-					team.getAutoArena().setTeam2( null );
+					team.getAutoArena().setTeam1(null);
+				} else if(team.getRole() == PlayerRole.Team2){
+					team.getAutoArena().setTeam2(null);
 				}
-				team.getAutoArena().getWgkArena().getReseter().cleanSide( side );
+			} 
+			if (wgkArena.getState() == State.Setup) {
+				GroupSide side = team.getRole() == PlayerRole.Team1 ? GroupSide.Team1 : GroupSide.Team2;
+				team.getAutoArena().getWgkArena().getReseter().cleanSide(side);
+				this.plugin.getRepo().getWarGear().getScoreboard().removeTeamMember(wgkArena, wgkArena.getGroupManager().getGroupMember(event.getPlayer()), team.getRole());
+				wgkArena.getGroupManager().getGroupOfPlayer(event.getPlayer()).remove(event.getPlayer());
+				
 			}
 		}
 	}
